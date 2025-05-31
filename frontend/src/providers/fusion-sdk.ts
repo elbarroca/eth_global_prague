324c0dbf6708fec36696843315fcc03ee9fef18e
pdrosalba <pedro.dandrea.rpd@gmail.com>
2025-05-31 17:57:32 +0200

    a

--- frontend/src/providers/fusion-sdk.ts
+++ frontend/src/providers/fusion-sdk.ts
@@ -6,7 +6,9 @@ import { Web3Like } from "./web3-provider";
 const web3 = new Web3(process.env.RPC_URL);
 
 const sdk = new SDK({  
-    url: 'https://localhost:5000/fusion-plus',  
+    url: process.env.BACKEND_URL as string,  
     authKey: process.env.ONE_INCH_API_KEY,  
     blockchainProvider: new PrivateKeyProviderConnector(process.env.PRIVATE_KEY as string, web3 as Web3Like)  
-})  
+}) 
+
+export default sdk;
